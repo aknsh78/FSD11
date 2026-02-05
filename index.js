a79026af2b71e2e8f4fcd9c21e31d57efe4ce1af
aknsh78 <akanshakukshal@gmail.com>
2026-02-05 16:20:58 +0530

    update

--- index.js
+++ index.js
@@ -1,1 +1,65 @@
-console.log("Hello")
+console.log("Hello");
+console.log(5-1);
+var a=30;
+var a=89;
+console.log(a);
+console.log(typeof(a));
+let b=20;
+console.log(typeof(b));
+const c=10;
+console.log(typeof(c));
+d=78;
+console.log(typeof(d));
+
+let num=100;
+let str="Hello there!"
+let bool=true;
+let nul=null;
+let und;
+console.log(typeof(num));
+console.log(typeof(str));
+console.log(typeof(nul));
+console.log(typeof(und));
+console.log(typeof(bool));
+let k=80;
+
+console.log(k);
+function add(p,q)
+{
+    return p+q;
+}
+console.log(add(10,20));
+const multiply=(p,q)=>p*q;
+console.log("arrow function",multiply(5,5));
+
+let arr=[10,20,30,40];
+console.log(arr);
+console.log(arr[0]);
+arr.push(23);
+console.log(arr);
+arr.pop();
+console.log(arr);
+let today_date=new Date();
+console.log(today_date);
+console.time("test");
+for(let i=0;i<10000;i++)
+{
+    
+
+}
+console.timeEnd("test");
+console.group()
+console.count("a")
+console.count("a")
+console.count("a")
+console.count("b")
+console.count("a")
+console.count("a")
+console.count("a")
+console.count("b")
+console.count("b")
+console.count("b")
+
+
+
+
